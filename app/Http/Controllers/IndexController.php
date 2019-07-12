f22d1904c5c162e38a7569a625edcf98edd76bdc
Kairat Ubukulov <kairat_ubukulov@mail.ru>
2019-07-12 09:50:43 +0600

    fix

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Category;
+use App\Product;
 use Illuminate\Http\Request;
 
 class IndexController extends BaseController
@@ -29,6 +30,47 @@ class IndexController extends BaseController
 
     public function lead(Request $request)
     {
-        //dd($request->all());
+        $first_name = $request->input('first_name');
+        $phone_number = $request->input('phone_number');
+        $comments = $request->input('comments');
+        $product_id = (int) $request->input('product_id');
+        if ($product_id != 0) {
+            $product = Product::find($product_id);
+            $p_title = $product->title;
+            $message = "
+            <html>
+                <head>
+                    <title>Новая заявка</title>
+                </head>
+                <body>
+                   <p>Имя: $first_name</p>
+                   <p>Телефон: $phone_number</p>
+                   <p>Комментарии: $comments</p>
+                   <p>Товар: $p_title</p> 
+                </body>
+            </html>    
+            ";
+        } else {
+            $message = "
+            <html>
+                <head>
+                    <title>Новая заявка</title>
+                </head>
+                <body>
+                   <p>Имя: $first_name</p>
+                   <p>Телефон: $phone_number</p>
+                   <p>Комментарии: $comments</p> 
+                </body>
+            </html>    
+            ";
+        }
+        $to = 'info@metaloprokat.kz';
+        $subject = 'Новая заявка';
+        $headers = "MIME-Version: 1.0\r\n";
+        $headers .= "Content-Type: text/html; charset=utf-8\r\n";
+
+        $headers .= "From: Новая заявка <info@metaloprokat.kz>\r\n";
+        mail($to, $subject, $message, $headers);
+        return redirect()->back();
     }
 }
