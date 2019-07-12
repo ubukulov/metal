299a14c6910c541c8088e9a7ed11aa4cb9276014
Kairat Ubukulov <kairat_ubukulov@mail.ru>
2019-07-12 10:30:14 +0600

    fix

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -74,7 +74,7 @@ class IndexController extends BaseController
         $headers = "MIME-Version: 1.0\r\n";
         $headers .= "Content-Type: text/html; charset=utf-8\r\n";
 
-        $headers .= "From: <info@metaloprokat.kz>\r\n";
+        $headers .= "From: <order@metaloprokat.kz>\r\n";
         mail($to, $subject, $message, $headers);
         return redirect()->back();
     }
